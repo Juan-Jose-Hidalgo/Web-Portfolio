95fab2d7730a385d3909b9ab1a13935a30d736eb
Juan Hidalgo <juanhidalgoyanez@gmail.com>
2023-06-08 18:34:27 +0200

    feat(home): :sparkles: Add custom scroll bar and animations with AOS and animate.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Aos from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Personal-Portfolio.v2';
+
+  ngOnInit(): void {
+    Aos.init({
+      offset: 120, // offset (in px) from the original trigger point
+      duration: 500, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: true, // whether elements should animate out while scrolling past them
+      anchorPlacement: 'top-bottom',
+    });
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { NgScrollbar } from 'ngx-scrollbar';
+import { SmoothScrollToOptions } from 'ngx-scrollbar/smooth-scroll';
 import { getProjectsAction } from 'src/app/state/actions/projects.actions';
 import { AppState } from 'src/app/state/app.state';
+import * as Aos from 'aos';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(NgScrollbar) scrollable!: NgScrollbar;
+  @ViewChild('about') about!: ElementRef;
+  @ViewChild('contact') contact!: ElementRef;
+  @ViewChild('home') home!: ElementRef;
+  @ViewChild('projects') projects!: ElementRef;
+
+  scrollOptions: SmoothScrollToOptions = {
+    duration: 800,
+    easing: { x1: 0, y1: 0, x2: 0, y2: 0.8 },
+  }
 
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
     this.store.dispatch(getProjectsAction());
+  }
+
+  ngAfterViewInit(): void {
+    this.scrollable.scrolled.subscribe(() => Aos.refresh())
+  }
+
+  scrollToSection(section: string) {
+    this.scrollable.scrollToElement(this[section], this.scrollOptions);
   }
 }
